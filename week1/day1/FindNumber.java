8628b628f7a419928de3403eda7100c6bd829a38
Pratyusha1108 <147968501+Pratyusha1108@users.noreply.github.com>
2023-10-15 12:19:39 +0530

    Week1 HomeAssignment

---
+++ week1/day1/FindNumber.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class FindNumber 
+{
+public static void main(String[] args) 
+{
+	int number=45;
+	if (number>0 )
+	{
+		System.out.println("number is positive");
+	}
+	else
+	{
+		System.out.println("number is negative");
+		}
+		
+	
+}
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox 
+{
+public static void main(String[] args) 
+{
+	float browserVersion=100.2f;
+	String browserName="firefox";
+	boolean isPageloaded=true;
+	int numberofbrowsers=1998;
+	char browserLogo='f';
+	
+	System.out.println("browserversion is:"+browserVersion);
+	System.out.println("browsername is:"+browserName);
+	System.out.println("browser is pageloaded:"+isPageloaded);
+	System.out.println("browserlogo is:"+browserLogo);
+	
+}
+}
